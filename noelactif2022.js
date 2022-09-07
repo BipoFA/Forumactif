c0670160ee5721028479eb5e32f9a324823e3c4b
BipoFA <52362400+BipoFA@users.noreply.github.com>
2022-09-07 05:57:42 +0200

    Update noelactif2022.js

--- noelactif2022.js
+++ noelactif2022.js
@@ -5,12 +5,12 @@ $(function() {
 $(function() {
     $('.musique').click(function() {
         if(!$('.footer_musique iframe').length) {
-            $('.footer_musique').html('<iframe width="0" height="0" src="https://www.youtube.com/embed/R1gskElaLNo?autoplay=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>');
-            $('header.page-header .musique span.volume').html('<i class="fa fa-volume-off"></i>')
-        } 
-        else {
             $('.footer_musique iframe').remove();
             $('header.page-header .musique span.volume').html('<i class="fa fa-volume-up"></i>')
+        } 
+        else {
+            $('.footer_musique').html('<iframe width="0" height="0" src="https://www.youtube.com/embed/R1gskElaLNo?autoplay=1" frameborder="0" allow="autoplay" allowfullscreen></iframe>');
+            $('header.page-header .musique span.volume').html('<i class="fa fa-volume-off"></i>')
         }
     });
 });
